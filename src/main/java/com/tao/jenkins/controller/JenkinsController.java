fae8e8a6ce993084b623bb00ed788664f7ec99e3
wentao <838737446@qq.com>
2020-09-29 21:27:42 +0800

    git init

---
+++ src/main/java/com/tao/jenkins/controller/JenkinsController.java
@@ -0,0 +1,15 @@
+package com.tao.jenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("jenkins")
+@RestController
+public class JenkinsController {
+
+    @GetMapping("test")
+    public String test(){
+        return "测试jenkins1";
+    }
+}
